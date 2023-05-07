15a26ec858237854b9e12106aa3844002a6b5223
brilu-22 <231214@virtualwindow.co.za>
2023-05-07 11:30:50 +0200

    Project 101
    the first javascript code

---
+++ js/index.js
@@ -0,0 +1,15 @@
+let tim = 10 
+let jane = 23 
+let john = 10 
+
+tim = tim + 10 
+
+let teamTotal = tim + jane
+let victory = john / 2 
+let total = teamTotal + victory
+ 
+
+
+let answer = "tim & jane now have " + total + " marbles "
+
+console.log(answer)
